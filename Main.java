c9f151c21e077a142b9e98b513c96351f5fb828b
Felipe Valenzuela <felipeivp@gmail.com>
2022-11-14 20:57:02 -0300

    Tarea Resuelta

--- Main.java
+++ Main.java
@@ -1,16 +1,27 @@
 public class Main {
-public static void main(String[] args) {
-    suma(10, 20, 50); 
+    public static void main(String[] args) {
+        suma(10, 20, 50);
+        Coche miCoche = new Coche();
+        miCoche.IncrementarPuertas();
 
-    
+        System.out.println(miCoche.puertas);
+
+
+    }
+
+    public static void suma(int a, int b, int c) {
+        int resultado;
+        resultado = a + b + c;
+
+        System.out.println(resultado);
+    }
+
+  
 }
+class Coche {
+    public int puertas = 4;
 
-public static void suma(int a, int b, int c){
-    int resultado;
-    resultado = a + b + c;
-
-    System.out.println(resultado);
-}
-
-
+    public void IncrementarPuertas() {
+        this.puertas ++;
+    }
 }
